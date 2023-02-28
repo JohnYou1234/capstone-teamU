dd0d9d62091c4d908b4b4e03ec221a24002be539
John <yyoungjune@gmail.com>
2023-02-28 03:23:23 -0800

    added error messages to see what's happening

--- config/db.js
+++ config/db.js
@@ -5,20 +5,25 @@ dbConnect().catch((err) => console.log(err));
 let db = {}
 // Connect to MongoDB
 async function dbConnect() {
-    await mongoose.connect(process.env.MONGODB_URL)
-    // Log successful connection
-    console.log("Successfully connected to the database!")
-    const PostSchema = new mongoose.Schema({
-        title: String,
-        content: String,
-        bgColor: String,
-        author: String,
-        category: String,
-        type: String,
-        date: { type: Date, default: Date.now }
-    });   
-    db.Post = mongoose.model('Posts', PostSchema)
-    console.log("Created DB Schemas and Models")
+    try {
+        await mongoose.connect(process.env.MONGODB_URL)
+        // Log successful connection
+        console.log("Successfully connected to the database!")
+        const PostSchema = new mongoose.Schema({
+            title: String,
+            content: String,
+            bgColor: String,
+            author: String,
+            category: String,
+            type: String,
+            date: { type: Date, default: Date.now }
+        });   
+        db.Post = mongoose.model('Posts', PostSchema)
+        console.log("Created DB Schemas and Models")
+    } catch (err) {
+        console.log("Error connecting to database")
+        console.log(err)
+    }
 }
 
 export default db;
